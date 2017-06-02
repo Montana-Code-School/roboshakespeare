b76227be2fbd6c3d921dd68343d65964bdab3c3b
Nathaniel Smith <nathanieltsmith@gmail.com>
2017-06-02 08:24:23 -0600

    fiend counter

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-var fs = require('fs');
-fs.readFile( __dirname + '/shakespeare.txt', function (err, data) {
-  if (err) {
+const fs = require('fs');
+
+
+const processFile = (err, data) => {
+  if(err) {
     throw err; 
   }
-  console.log(data.toString());
-});
+  console.log(youFiend(data.toString().toLowerCase()))
+}
+
+
+fs.readFile( __dirname + '/shakespeare.txt', processFile);
+
+console.log('Has the file been read yet')
+
+const fiendCleaner = (str) => {
+	return str.replace(/[^A-Za-z]+/g, "")
+}
+
+const fiendCount = (total, next) => {
+	const count = next === "romeo"  ? 1 : 0
+	return count + total
+}
+
+const youFiend = (str) => {
+	const arrayShakes = str.split(" ").map(fiendCleaner)
+	return arrayShakes.reduce(fiendCount, 0)
+}
+
+
+
